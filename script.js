0c175c7cb295550651731da046519d149706769c
Matheus Arantes <125316650+Programmerarantes@users.noreply.github.com>
2023-05-24 18:20:07 -0300

    menu responsivo

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-menu = document.querySelector(".menu")
-menu.onclick = function() {
-    navBar = document.querySelector(".nav-bar")
-    navBar.classList.toggle("active")
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById("nav");
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('arial-label', 'Abrir Menu')
+    }
 }
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
